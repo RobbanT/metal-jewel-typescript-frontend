e5905caaa1d117c0b0a763af9241ea503dd21016
Roberto Tencic <roberto.tencic@outlook.com>
2025-03-12 11:28:15 +0100

    Uppdatering.

--- src/misc/Canvas.ts
+++ src/misc/Canvas.ts
@@ -30,16 +30,16 @@ class Canvas {
         });
 
         this.canvas.addEventListener("touchmove", (event) => {
-            this._inputData.position = new Vector((event.touches[0].pageX - this.canvas.offsetLeft) / gameScale.x, (event.touches[0].pageY - this.canvas.offsetTop) / gameScale.y);
+            //this._inputData.position = new Vector((event.touches[0].pageX - this.canvas.offsetLeft) / gameScale.x, (event.touches[0].pageY - this.canvas.offsetTop) / gameScale.y);
         });
 
         this.canvas.addEventListener("touchstart", (event) => {
-            this._inputData.touchStarted = true;
+            //this._inputData.touchStarted = true;
         });
 
         this.canvas.addEventListener("touchend", (event) => {
-            this._inputData.touchEnded = true;
-            this._inputData.touchStarted = false;
+            //this._inputData.touchEnded = true;
+            //this._inputData.touchStarted = false;
         });
     }
 
